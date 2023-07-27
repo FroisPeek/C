7d811b9f5ae9e6c4f5a58e13cf82a192e8b9ffee
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-27 15:36:46 -0300

    Add files via upload

---
+++ Exercicios/02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int r = 0;
+volume = 0;
+
+int main(){
+    printf("Faça o programa que calcule o volume de uma esfera de raio R. O usuário fornecerá o dado necessário. \nOnde: volume = 4.r3/3");
+    printf("\nInforme o valor do raio da esfera: ");
+    scanf("%d", &r);
+    volume = 4/3 * (pow(r, 3));
+    printf("%d", volume);
+}
+W
