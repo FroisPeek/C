0675e731c4dbfdabc5373130bc23aea744cc5e46
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-30 16:15:45 -0300

    Update and rename 02.c to treinando03.c

--- Exercicios/treinando03.c
+++ Exercicios/treinando03.c
@@ -5,7 +5,7 @@ int r = 0;
 volume = 0;
 
 int main(){
-    printf("FaÁa o programa que calcule o volume de uma esfera de raio R. O usu·rio fornecer· o dado necess·rio. \nOnde: volume = 4.r3/3");
+    printf("Fa√ßa o programa que calcule o volume de uma esfera de raio R. O usu√°rio fornecer√° o dado necess√°rio. \nOnde: volume = 4.r3/3");
     printf("\nInforme o valor do raio da esfera: ");
     scanf("%d", &r);
     volume = 4/3 * (pow(r, 3));
