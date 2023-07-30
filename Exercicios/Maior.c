0e5132ecf2b4d317a137a268c6335b4d40808c66
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-30 16:13:47 -0300

    Update and rename 08.c to Maior.c

--- Exercicios/Maior.c
+++ Exercicios/Maior.c
@@ -7,17 +7,17 @@ int num2 = 0;
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    printf("Faça um Programa que peça dois números e imprima o maior deles.");
+    printf("FaÃ§a um Programa que peÃ§a dois nÃºmeros e imprima o maior deles.");
     printf("\nInforme o primeiro valor: ");
     scanf("%d", &num1);
     printf("Informe o segundo valor: ");
     scanf("%d", &num2);
 
     if (num1 > num2){
-        printf("O valor maior é %d", num1);
+        printf("O valor maior Ã© %d", num1);
     }
     else if (num2 > num1){
-        printf("O valor maior é %d", num2);
+        printf("O valor maior Ã© %d", num2);
     }
     else {
         printf("Os dois valores sao iguais!");
