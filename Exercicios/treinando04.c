97b26acedf5081af843bcda1c824bd340f386224
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-07-30 22:48:00 -0300

    Add files via upload

---
+++ Exercicios/treinando04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Programa pra entender como funciona um vetor.
+
+int main(){
+    char nome [20];
+
+    printf("Informe o nome do usuario: ");
+    scanf("%s", nome);
+
+    printf("A primeira letra do seu nome: %c", nome[0]);
+}
