aa8497e3cb22859d14e18c538dda8adc86bbf349
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-08-03 15:41:39 -0300

    Add files via upload

---
+++ Exercicios/FOR/001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Exercicios para treinar a estrutura de repeticao for.
+// Imprimir os valores de 1 a 10:
+
+int i = 0;
+int main(){
+    for(i = 1; i <= 10; i++){
+    printf("%d\n", i);
+    }
+}
