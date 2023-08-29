620f0436f12af35547b4c0cfe72f7ad5b1394821
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-08-29 12:19:39 -0300

    Add files via upload

---
+++ CursoC/aula001/aula001.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int main(){
+    /*  // comentarios da linguagem C. --> Estou comentando todos os códigos de exercício.
+    int x;
+    int y;
+    printf("Alo mundo!\n");
+    printf("Informe o primeiro valor: ");
+    scanf("%d", &x);
+    printf("Informe o segundo valor: ");
+    scanf("%d", &y);
+    printf("\nA soma desse dois valores: %d", x + y);
+    */
+
+    /*
+    int x;
+    printf("Informe um valor: ");
+    scanf("%d", &x);
+    printf("O valor informado foi: %d", x);
+    */
+
+    /*
+    int x;
+    printf("Informe um valor: ");
+    scanf("%d", &x);
+    if (x == 0){
+        printf("O valor informado nulo!");
+    } else if (x % 2 == 0){
+        printf("O valor informado par!");
+    } else {
+        printf("Valor informado impar!");
+    }
+    */
+
+    /*
+    int numero;
+    printf("Informe um valor de 1 a 3: ");
+    scanf("%d", &numero);
+    switch (numero){
+    case 1:
+        printf("Valor informado: 1");
+    case 2:
+        printf("Valor informado: 2");
+    case 3:
+        printf("Valor informado: 3");
+    default:
+        printf("Valor invalido");
+    }
+    */
+
+    /*
+    int x; int y;
+    printf("Informe o valor de x: ");
+    scanf("%d", &x);
+    printf("Informe o valor de y: ");
+    scanf("%d", &y);
+    if (x == y){
+        printf("Os valores sao iguais!");
+    } else {
+        printf("Os valores são diferentes!");
+    }
+    */
+
+    /*
+    int x;
+    printf("Informe um valor: ");
+    scanf("%d", &x);
+    if (x % 5 == 0){
+        printf("Divisivel por 5.");
+    } else {
+        printf("Nao divisivel por 5.");
+    }
+    */
+
+    /*
+    int time1;
+    int time2;
+    printf("Informe o placar do primeiro time: ");
+    scanf("%d", &time1);
+    printf("Informe o placar do segundo time: ");
+    scanf("%d", &time2);
+    if (time1 == time2){
+        printf("O jogo deu empate!");
+    } else if (time1 > time2){
+        printf("O time 1 ganhou a partida!");
+    } else{
+        printf("O time 2 ganhou a partida!");
+    }
+    */
+
+
+    return 0;
+}
