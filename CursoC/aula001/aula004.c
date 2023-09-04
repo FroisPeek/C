bab05ccbb019c68e316255ebb27a7c3d9811164f
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-09-04 11:44:18 -0300

    Add files via upload

---
+++ CursoC/aula001/aula004.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+    // Aula sobre a estrutura de repetição While.
+
+int main(){
+    /*
+    int x = 100;
+    while(x <= 200){
+        if (x % 2 == 0){
+            printf("%d\n", x);
+        }
+        x++;
+    }
+    */
+
+    /*
+    int ct = 0;
+    int x = 0;
+    int y = 0;
+    printf("Informe o primeiro valor: ");
+    scanf("%d", &x);
+    printf("\nInforme o segundo valor: ");
+    scanf("%d", &y);
+    while (x <= y){
+        if(x % 2 == 0 ){
+            ct++;
+        }
+        x++;
+    }
+    printf("O pares entre os valores: %d", ct);
+    */
+
+    /*
+    int soma = 0;
+    int x = 0;
+    int y = 0;
+    printf("Informe o primeiro valor: ");
+    scanf("%d", &x);
+    printf("\nInforme o segundo valor: ");
+    scanf("%d", &y);
+    while (x <= y){
+        if(x % 2 == 0 ){
+            soma += x;
+        }
+        x++;
+    }
+    printf("A soma dos valores: %d", soma);
+    */
+
+}
